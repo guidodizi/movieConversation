fcabcaa47e46509a6ff3eee3a1d913075e8c3d86
Guido Dizioli <guidodizi@gmail.com>
2018-03-08 12:18:56 -0300

    bugfix

--- handleResponse.js
+++ handleResponse.js
@@ -17,38 +17,39 @@ function handleTemplateResponse(context, sender_psid) {
     //Generate elements
     switch (watson_payload.data) {
         case "GENERIC_TEMPLATE_MOVIES": {
-            database.movies.forEach(movie => {
-                //Search if its displayed on selected date
-                database.schedules.contentCinemaShows.forEach(contentCinema => {
-                    //Find content for selected movie
-                    if (contentCinema.contentId === movie.id) {
-                        contentCinema.cinemaShows.forEach(cinemaShow => {
-                            //Check if cinemas is for selected place
-                            cinemaShow.shows.some(show => {
-                                //Check if show is for selected date
-                                var show_date = moment(show.date).dayOfYear();
-                                var user_date = moment(context.data.date).dayOfYear();
-                                if (show_date === user_date) {
-                                    response.attachment.payload.elements.push({
-                                        title: movie.content.title,
-                                        subtitle: movie.content.synopsis,
-                                        image_url: movie.content.posterUrl,
-                                        buttons: [{
-                                                type: "postback",
-                                                title: "Elegir",
-                                                payload: movie.content.title
-                                            }]
-
-                                    })
-                                    //Exit iteration
-                                    return true;
-                                }
-                            });
+            const id_movie_for_date = [];
+            database.schedules.contentCinemaShows.forEach(contentCinema => {
+                //Find content for selected movie
+                    contentCinema.cinemaShows.forEach(cinemaShow => {
+                        //Check if cinemas is for selected place
+                        cinemaShow.shows.some(show => {
+                            //Check if show is for selected date
+                            var show_date = moment(show.date).dayOfYear();
+                            var user_date = moment(context.data.date).dayOfYear();
+                            if (show_date === user_date) {
+                                id_movie_for_date.push(contentCinema.contentId)
+                                //Exit iteration
+                                return true;
+                            }
                         });
-
-                    }
-                })
-            })
+                    });
+        
+            });
+            database.movies.forEach( movie => {
+                if (id_movie_for_date.indexOf(movie.content.id) !== -1){
+                    response.attachment.payload.elements.push({
+                        title: movie.content.title,
+                        subtitle: movie.content.synopsis,
+                        image_url: movie.content.posterUrl,
+                        buttons: [{
+                            type: "postback",
+                            title: "Elegir",
+                            payload: movie.content.title
+                        }]
+                
+                    })
+                }
+            });
             if (!response.attachment.payload.elements.length){
                 response = { text: `No encontré peliculas para ${context.data.date_synonym}. Recuerda que la cartelera cambia todos los jueves.` };
                 userContext.updateUserContext(sender_psid, {});            
