9598cfaee254acc82f36ea338299134c72dba8d3
Guido Dizioli <guidodizi@gmail.com>
2018-03-09 11:25:52 -0300

    fix on answer body

--- handleResponse/handleResponse.js
+++ handleResponse/handleResponse.js
@@ -351,7 +351,7 @@ function callSendAPI(sender_psid, body, callback) {
     // Send the HTTP request to the Messenger Platform
     request({
         "uri": "https://graph.facebook.com/v2.6/me/messages",
-        "qs": { "access_token": "EAAFBoi5d2s4BAMJSjShOY9dUIS8g7SsqiZB8i9S9GlbyKn4Ygu07KVY2uclO0gmTeTzy0Rwl7ms8ZAWyOCqbfkaB4y8IAW7ZAhZCell3oKUfl35mkcHmZBh37AFHXvugD1JTQC0FEDOl2fwvBhA2kdaQn7SXdsTbdHRYmSLu4TQZDZD" },
+        "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
         "method": "POST",
         "json": body
     }, (err, res, body) => {
@@ -375,7 +375,9 @@ function sendAPI(sender_psid, response, options = {}) {
         recipient: { 
             id: sender_psid 
         }, 
-        message: response 
+        message: {
+            text: response
+        } 
     };
     if (with_typing) {
         let typing_on_body = { 
