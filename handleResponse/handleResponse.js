7afc249c0f4bafbd6f49c3301f187cef9d1aa75c
Guido Dizioli <guidodizi@gmail.com>
2018-03-09 14:41:22 -0300

    try catch

--- handleResponse/handleResponse.js
+++ handleResponse/handleResponse.js
@@ -17,20 +17,25 @@ module.exports = function handleResponse(sender_psid, context, text_response) {
 
     console.log('CONTEXT: ' + JSON.stringify(context, null))
     console.log('TEXT RESPONSE: ' + text_response)
-    switch (message_type) {
-        case "template": {
-            handleTemplateResponse(sender_psid, text_response, context);
-            break;
+    try {
+        switch (message_type) {
+            case "template": {
+                handleTemplateResponse(sender_psid, text_response, context);
+                break;
+            }
+            case "quick_replies": {
+                handelQuickRepliesResponse(sender_psid, text_response, context);
+                break;
+            }
+            default: {
+                // Directly respond to user
+                sendAPI(sender_psid, { text: text_response }).catch((err) => { console.log(err); } );
+                break;
+            }
         }
-        case "quick_replies": {
-            handelQuickRepliesResponse(sender_psid, text_response, context);
-            break;
-        }
-        default: {
-            // Directly respond to user
-            sendAPI(sender_psid, { text: text_response }).catch((err) => { console.log(err); } );
-            break;
-        }
+    }
+    catch (err) {
+        console.log(err)
     }
 }
 
