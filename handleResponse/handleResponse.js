97d30b6c38a7c1f880808ed33af1c0e90e20d84f
Guido Dizioli <guidodizi@gmail.com>
2018-03-09 12:35:21 -0300

    handle response big refactor for code cleanness

--- handleResponse/handleResponse.js
+++ handleResponse/handleResponse.js
@@ -3,6 +3,7 @@ const database = require('../database');
 const userContext = require('../userContext');
 const constants = require('../constants');
 const templateResponse = require('./templateResponse');
+const quickRepliesResponse = require('./quickRepliesResponse');
 const { sendAPI, callSendAPI } = require('./sendAPI');
 
 /*
@@ -46,7 +47,7 @@ module.exports = function handleResponse(sender_psid, context, text_response) {
 *   data to put on template: context.payload.data
 *
 */
-async function handleTemplateResponse(sender_psid, text_response, context) {
+function handleTemplateResponse(sender_psid, text_response, context) {
     const watson_payload = context.payload;
 
     //Base response for templates
@@ -56,154 +57,23 @@ async function handleTemplateResponse(sender_psid, text_response, context) {
 
     //Generate elements depending on what string Watson gave on context.payload.data
     switch (watson_payload.data) {
-        case "GENERIC_TEMPLATE_MOVIES": {
-            // //Answer user that search has began
-            // await sendAPI(sender_psid, { text: "Déjame mostrarte..." });
-
-            // //ids of movies available for selected date
-            // const id_movie_for_date = [];
-            // database.schedules.contentCinemaShows.forEach(contentCinema => {
-            //     //Find content for selected movie
-            //     contentCinema.cinemaShows.forEach(cinemaShow => {
-            //         //Check if cinemas is for selected place
-            //         cinemaShow.shows.some(show => {
-            //             //Check if show is for selected date
-            //             var show_date = moment(show.date).dayOfYear();
-            //             var user_date = moment(context.data.date).dayOfYear();
-            //             if (show_date === user_date) {
-            //                 id_movie_for_date.push(contentCinema.contentId)
-            //                 //Exit iteration
-            //                 return true;
-            //             }
-            //         });
-            //     });
-
-            // });
-            // database.movies.forEach(movie => {
-            //     if (id_movie_for_date.indexOf(movie.content.id) !== -1) {
-            //         response.attachment.payload.elements.push({
-            //             title: movie.content.title,
-            //             subtitle: movie.content.synopsis,
-            //             image_url: movie.content.posterUrl,
-            //             buttons: [{
-            //                 type: "postback",
-            //                 title: "Elegir",
-            //                 payload: movie.content.title
-            //             }]
-
-            //         })
-            //     }
-            // });
-            // if (!response.attachment.payload.elements.length) {
-            //     response = { text: `No encontré peliculas para ${context.data.date_synonym}. Recuerda que la cartelera cambia todos los jueves.` };
-            //     userContext.updateUserContext(sender_psid, {});
-            // }
+        case constants.GENERIC_TEMPLATE_MOVIES: {
             templateResponse[constants.GENERIC_TEMPLATE_MOVIES](sender_psid, response, context);
             break;
         }
-        case "GENERIC_TEMPLATE_MOVIES_GENRE": {
-            database.movies.forEach(movie => {
-                //Example : ['comedia', 'accion']
-                const movie_genres = movie.content.genre.split(', ').map(genre => genre.toLowerCase());
-                if (movie_genres.indexOf(context.data.genre.toLowerCase()) !== -1) {
-                    // Movie contains selected genre
-                    response.attachment.payload.elements.push(
-                        {
-                            title: movie.content.title,
-                            subtitle: movie.content.synopsis,
-                            image_url: movie.content.posterUrl,
-                            buttons: [
-                                {
-                                    type: "postback",
-                                    title: "Elegir",
-                                    payload: movie.content.title
-                                }
-                            ]
-
-                        }
-                    )
-                }
-
-            });
+        case constants.GENERIC_TEMPLATE_MOVIES_GENRE: {
+            templateResponse[constants.GENERIC_TEMPLATE_MOVIES_GENRE](sender_psid, response, context);            
             break;
         }
-        case "GENERIC_TEMPLATE_MOVIES_GENRE_PLACE": {
-            database.movies.forEach(movie => {
-                //Example : ['comedia', 'accion']
-                const movie_genres = movie.content.genre.split(', ').map(genre => genre.toLowerCase());
-
-                // Movie contains selected genre
-                if (movie_genres.indexOf(context.data.genre.toLowerCase()) !== -1) {
-
-                    movie.cinemasIds.forEach(cinema => {
-                        //Movie is on selected place
-                        if (cinema.name.toLowerCase() === context.data.place.toLowerCase()) {
-                            response.attachment.payload.elements.push(
-                                {
-                                    title: movie.content.title,
-                                    subtitle: movie.content.synopsis,
-                                    image_url: movie.content.posterUrl,
-                                    buttons: [
-                                        {
-                                            type: "postback",
-                                            title: "Elegir",
-                                            payload: movie.content.title
-                                        }
-                                    ]
-
-                                }
-                            )
-                        }
-                    })
-                }
-
-            });
+        case constants.GENERIC_TEMPLATE_MOVIES_GENRE_PLACE: {
+            templateResponse[constants.GENERIC_TEMPLATE_MOVIES_GENRE_PLACE](sender_psid, response, context);                        
             break;
         }
-        case "GENERIC_TEMPLATE_SCHEDULE": {
-            //Find id of selected movie
-            const selected_movie = database.movies_id.find(movie => {
-                return movie.title === context.data.movie;
-            })
-            database.schedules.contentCinemaShows.forEach(contentCinema => {
-                //Find content for selected movie
-                if (contentCinema.contentId === selected_movie.id) {
-                    contentCinema.cinemaShows.forEach(cinemaShow => {
-                        //Check if cinemas is for selected place
-                        if (cinemaShow.cinema.name === context.data.place) {
-                            cinemaShow.shows.forEach(show => {
-                                //Check if show is for selected date
-                                var show_date = moment(show.date).dayOfYear();
-                                var user_date = moment(context.data.date).dayOfYear();
-                                if (show_date === user_date) {
-                                    response.attachment.payload.elements.push(
-                                        {
-                                            title: `Día: ${show.dateToDisplay}   Hora: ${show.timeToDisplay}`,
-                                            subtitle: show.formatLang,
-                                        }
-                                    )
-                                }
-                            })
-                        }
-                    });
-
-                }
-            })
-            if (!response.attachment.payload.elements.length) {
-                response = { text: `No encontré horarios para ${context.data.date_synonym}. Recuerda que la cartelera cambia todos los jueves.` };
-            }
-            //End of dialog, clear context
-            userContext.updateUserContext(sender_psid, {});
-
+        case constants.GENERIC_TEMPLATE_SCHEDULE: {
+            templateResponse[constants.GENERIC_TEMPLATE_SCHEDULE](sender_psid, response, context);                                    
             break;
         }
     }
-    /**
-     * Response is now nurtured for user to receive it, send it to user
-     */
-    //console.log('\n GENERATED RESPONSE: ' + JSON.stringify(response, null, 1))
-
-    //await sendAPI(sender_psid, response, { with_typing: true })
 }
 /*
 * Handle responses which we offer Quick Replies. 
@@ -211,136 +81,24 @@ async function handleTemplateResponse(sender_psid, text_response, context) {
 * data for quick replies:  context.payload.data
 *
 */
-async function handelQuickRepliesResponse(sender_psid, text_response, context) {
+function handelQuickRepliesResponse(sender_psid, text_response, context) {
     const watson_payload = context.payload;
     let response = { text: text_response, quick_replies: [] };
 
     switch (watson_payload.data) {
-        case "QUICK_REPLIES_LOCATIONS": {
-            //Find id of selected movie
-            const { id } = database.movies_id.find(movie => {
-                return movie.title === context.data.movie;
-            });
-            database.schedules.contentCinemaShows.forEach(contentCinema => {
-                //Find content for selected movie
-                if (contentCinema.contentId === id) {
-                    //Save on context the cinema options for selected movie
-                    const data_to_change = {
-                        cinemas: []
-                    };
-                    contentCinema.cinemaShows.forEach(cinemaShow => {
-                        cinemaShow.shows.some(show => {
-                            var show_date = moment(show.date).dayOfYear();
-                            var user_date = moment(context.data.date).dayOfYear();
-                            // shows for selected date
-                            if (show_date === user_date) {
-                                response.quick_replies.push({
-                                    content_type: "text",
-                                    title: show.cinemaName,
-                                    payload: show.cinemaName,
-                                });
-                                //Modify user watson context to update what locations are availabe for that movie
-                                data_to_change.cinemas.push(show.cinemaName);
-                                // Exit cinemaShow.shows iteration as it already found 
-                                // a show for selected date on that cinema
-                                return true;
-                            }
-                        })
-                    });
-                    //Update user context with cinema
-                    userContext.mergeUserContext(sender_psid, data_to_change);
-                }
-            });
-            //No posible response
-            if (!response.quick_replies.length) {
-                response = { text: `Lo siento! No hay lugares donde pasen ${context.data.movie}` };
-            }
+        case constants.QUICK_REPLIES_LOCATIONS: {
+            quickRepliesResponse[constants.QUICK_REPLIES_LOCATIONS](sender_psid, response, context)
             break;
         };
-        case "QUICK_REPLIES_DATE": {
-            const weekdays = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"]
-            //Array so as not to repeat days of show => 2 Saturday options
-            const days_of_show = [];
-            //Find id of selected movie
-            const { id } = database.movies_id.find(movie => {
-                return movie.title === context.data.movie;
-            });
-            database.schedules.contentCinemaShows.forEach(contentCinema => {
-                //Find content for selected movie
-                if (contentCinema.contentId === id) {
-                    contentCinema.cinemaShows.forEach(cinemaShow => {
-                        //Check if cinemas is for selected place
-                        cinemaShow.shows.forEach(show => {
-                            //Check if show is for selected date
-                            const show_date = moment(show.date);
-                            const now_date = moment().tz("America/Montevideo");
-                            if (show_date.dayOfYear() >= now_date.dayOfYear()) {
-                                if (days_of_show.indexOf(show_date.weekday()) === -1) {
-                                    days_of_show.push(show_date.weekday());
-                                    response.quick_replies.push({
-                                        content_type: "text",
-                                        title: weekdays[show_date.weekday()],
-                                        payload: show_date.format("DD/MM/YYYY"),
-                                    });
-                                }
-                            }
-                        })
-                    });
-                }
-            });
-            //No posible response
-            if (!response.quick_replies.length) {
-                response = { text: `Lo siento! No hay días que pasen ${context.data.movie}` };
-            }
+        case constants.QUICK_REPLIES_DATE: {
+            quickRepliesResponse[constants.QUICK_REPLIES_DATE](sender_psid, response, context)            
             break;
         }
-        case "QUICK_REPLIES_DATE_PLACE": {
-            const weekdays = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"]
-            //Array so as not to repeat days of show => 2 Saturday options
-            const days_of_show = [];
-            //Find id of selected movie
-            const { id } = database.movies_id.find(movie => {
-                return movie.title === context.data.movie;
-            });
-            database.schedules.contentCinemaShows.forEach(contentCinema => {
-                //Find content for selected movie
-                if (contentCinema.contentId === id) {
-                    contentCinema.cinemaShows.forEach(cinemaShow => {
-                        //Check if cinemas is for selected place
-                        if (cinemaShow.cinema.name === context.data.place) {
-                            cinemaShow.shows.forEach(show => {
-                                //Check if show is for selected date
-                                const show_date = moment(show.date);
-                                const now_date = moment().tz("America/Montevideo");
-                                if (show_date.dayOfYear() >= now_date.dayOfYear()) {
-                                    if (days_of_show.indexOf(show_date.weekday()) === -1) {
-                                        days_of_show.push(show_date.weekday());
-                                        response.quick_replies.push({
-                                            content_type: "text",
-                                            title: weekdays[show_date.weekday()],
-                                            payload: show_date.format("DD/MM/YYYY"),
-                                        });
-                                    }
-                                }
-                            })
-                        }
-                    });
-                }
-            });
-            //No posible response
-            if (!response.quick_replies.length) {
-                response = { text: `Lo siento! No hay horarios para ${context.data.movie} en ${context.data.place}` };
-                // Clear conversation context
-                userContext.updateUserContext(sender_psid, {});
-            }
+        case constants.QUICK_REPLIES_DATE_PLACE: {
+            quickRepliesResponse[constants.QUICK_REPLIES_DATE_PLACE](sender_psid, response, context)            
             break;
         }
     };
-    /**
-     * Response is now nurtured for user to receive it, send it to user
-     */
-    console.log('\n GENERATED RESPONSE: ' + JSON.stringify(response, null, 1))
-    await sendAPI(sender_psid, response, { with_typing: true })
 }
 
 
--- handleResponse/templateResponse.js
+++ handleResponse/templateResponse.js
@@ -1,10 +1,13 @@
 const database = require('../database');
-const userContext = require('../userContext');
+const { getUserContext, mergeUserContext, updateUserContext } = require('../userContext');
 const constants = require('../constants');
 const moment = require('moment-timezone');
 const { sendAPI, callSendAPI } = require('./sendAPI');
 
-
+/**
+* Get movies based on a (date)
+*
+*/
 exports[constants.GENERIC_TEMPLATE_MOVIES] = async (sender_psid, response, context) => {
     //Answer user that search has began
     await sendAPI(sender_psid, { text: "Déjame mostrarte..." }).catch(err => { console.log(err); });
@@ -45,7 +48,7 @@ exports[constants.GENERIC_TEMPLATE_MOVIES] = async (sender_psid, response, conte
     });
     if (!response.attachment.payload.elements.length) {
         response = { text: `No encontré peliculas para ${context.data.date_synonym}. Recuerda que la cartelera cambia todos los jueves.` };
-        userContext.updateUserContext(sender_psid, {});
+        updateUserContext(sender_psid, {});
     }
 
     /**
@@ -53,4 +56,126 @@ exports[constants.GENERIC_TEMPLATE_MOVIES] = async (sender_psid, response, conte
     */
     console.log('\n GENERATED RESPONSE: ' + JSON.stringify(response, null, 1))
     await sendAPI(sender_psid, response, { with_typing: true }).catch(err => { console.log(err); });
-}
+};
+
+
+/*
+* Get movies based on (genre, date)
+*
+*/
+exports[constants.GENERIC_TEMPLATE_MOVIES_GENRE] = async (sender_psid, response, context) => {
+    database.movies.forEach(movie => {
+        //Example : ['comedia', 'accion']
+        const movie_genres = movie.content.genre.split(', ').map(genre => genre.toLowerCase());
+        if (movie_genres.indexOf(context.data.genre.toLowerCase()) !== -1) {
+            // Movie contains selected genre
+            response.attachment.payload.elements.push(
+                {
+                    title: movie.content.title,
+                    subtitle: movie.content.synopsis,
+                    image_url: movie.content.posterUrl,
+                    buttons: [
+                        {
+                            type: "postback",
+                            title: "Elegir",
+                            payload: movie.content.title
+                        }
+                    ]
+
+                }
+            )
+        }
+    });
+    /**
+    * Response is now nurtured for user to receive it, send it to user
+    */
+    console.log('\n GENERATED RESPONSE: ' + JSON.stringify(response, null, 1))
+    await sendAPI(sender_psid, response, { with_typing: true }).catch(err => { console.log(err); });
+};
+
+
+/*
+* Get movies based on (genre, date)
+*
+*/
+exports[constants.GENERIC_TEMPLATE_MOVIES_GENRE_PLACE] = async (sender_psid, response, context) => {
+    database.movies.forEach(movie => {
+        //Example : ['comedia', 'accion']
+        const movie_genres = movie.content.genre.split(', ').map(genre => genre.toLowerCase());
+
+        // Movie contains selected genre
+        if (movie_genres.indexOf(context.data.genre.toLowerCase()) !== -1) {
+            movie.cinemasIds.forEach(cinema => {
+                //Movie is on selected place
+                if (cinema.name.toLowerCase() === context.data.place.toLowerCase()) {
+                    response.attachment.payload.elements.push(
+                        {
+                            title: movie.content.title,
+                            subtitle: movie.content.synopsis,
+                            image_url: movie.content.posterUrl,
+                            buttons: [
+                                {
+                                    type: "postback",
+                                    title: "Elegir",
+                                    payload: movie.content.title
+                                }
+                            ]
+
+                        }
+                    )
+                }
+            })
+        }
+    });
+    /**
+    * Response is now nurtured for user to receive it, send it to user
+    */
+    console.log('\n GENERATED RESPONSE: ' + JSON.stringify(response, null, 1))
+    await sendAPI(sender_psid, response, { with_typing: true }).catch(err => { console.log(err); });
+};
+
+
+/**
+ * Get schedule for selected (date, movie, place)
+ * 
+ */
+exports[constants.GENERIC_TEMPLATE_SCHEDULE] = async (sender_psid, response, context) => {
+    //Find id of selected movie
+    const selected_movie = database.movies_id.find(movie => {
+        return movie.title === context.data.movie;
+    })
+    database.schedules.contentCinemaShows.forEach(contentCinema => {
+        //Find content for selected movie
+        if (contentCinema.contentId === selected_movie.id) {
+            contentCinema.cinemaShows.forEach(cinemaShow => {
+                //Check if cinemas is for selected place
+                if (cinemaShow.cinema.name === context.data.place) {
+                    cinemaShow.shows.forEach(show => {
+                        //Check if show is for selected date
+                        var show_date = moment(show.date).dayOfYear();
+                        var user_date = moment(context.data.date).dayOfYear();
+                        if (show_date === user_date) {
+                            response.attachment.payload.elements.push(
+                                {
+                                    title: `Día: ${show.dateToDisplay}   Hora: ${show.timeToDisplay}`,
+                                    subtitle: show.formatLang,
+                                }
+                            )
+                        }
+                    })
+                }
+            });
+
+        }
+    })
+    if (!response.attachment.payload.elements.length) {
+        response = { text: `No encontré horarios para ${context.data.date_synonym}. Recuerda que la cartelera cambia todos los jueves.` };
+    }
+    //End of dialog, clear context
+    updateUserContext(sender_psid, {});
+    /**
+    * Response is now nurtured for user to receive it, send it to user
+    */
+    console.log('\n GENERATED RESPONSE: ' + JSON.stringify(response, null, 1))
+    await sendAPI(sender_psid, response, { with_typing: true }).catch(err => { console.log(err); });
+};
